52821565f5ed97339cdab5f510c6557d1e68f291
ra260514 <isaclucas37@gmail.com>
2023-03-21 15:40:56 -0300

    Metodo para validar cpf adicionado

--- ProjetoMC322/src/classes/Cliente.java
+++ ProjetoMC322/src/classes/Cliente.java
@@ -63,4 +63,57 @@ public class Cliente {
 				"Endereco: " + endereco + "\n"
 				);
 	}
+	
+	private boolean is_equal_characters(String str) {
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(0) != str.charAt(i))
+				return false;
+		}
+		return true;
+	}
+	
+	private boolean has_valid_dv (String cpf) {
+		int i,j;
+		
+		//verifica primeiro dv
+		for (i = 0, j = 0; i < 9; i++)
+			j += (10 - i) * (Integer.parseInt(String.valueOf(cpf.charAt(i))));
+		if ((j % 11) == 1 || (j % 11) == 0) {
+			if (cpf.charAt(9) != 0)
+				return false;
+		}
+		else {
+			if ((11 - (j%11)) != (Integer.parseInt(String.valueOf(cpf.charAt(9)))))
+				return false;
+		}
+		
+		//verifica segundo dv
+		for (i = 0, j = 0; i < 10; i++)
+			j += (11 - i) * (Integer.parseInt(String.valueOf(cpf.charAt(i))));
+		if ((j % 11) == 1 || (j % 11) == 0) {
+			if (cpf.charAt(10) != 0)
+				return false;
+		}
+		else {
+			if ((11 - (j%11)) != (Integer.parseInt(String.valueOf(cpf.charAt(10)))))
+				return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean validarCPF() {
+		int n_digitos;
+		String cpf;
+		cpf = this.cpf.replaceAll("[^0-9]","");
+		
+		n_digitos = cpf.length();
+		if (n_digitos != 11)
+			return false;
+		
+		if (is_equal_characters(cpf))
+			return false;
+		
+		return has_valid_dv(cpf);
+	}
 }
